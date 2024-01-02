ff6986fa3877c83cd7d838523344979789efee19
kush-raj-nit061 <kushpvt1@gmail.com>
2024-01-02 23:38:16 +0530

    All Class Added

---
+++ app/src/main/java/com/ingray/samagam/Activity/AddPostsActivity.kt
@@ -0,0 +1,12 @@
+package com.ingray.samagam.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ingray.samagam.R
+
+class AddPostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_posts)
+    }
+}
---
+++ app/src/main/java/com/ingray/samagam/Activity/EventsOfClubsActivity.kt
@@ -0,0 +1,12 @@
+package com.ingray.samagam.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ingray.samagam.R
+
+class EventsOfClubsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events_of_clubs)
+    }
+}
