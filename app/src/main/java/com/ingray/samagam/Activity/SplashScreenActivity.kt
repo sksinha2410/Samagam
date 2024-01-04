2366366bcdfb1e99e68ec877f469e229b8359ee3
kush-raj-nit061 <kushpvt1@gmail.com>
2024-01-04 17:08:10 +0530

    SplashScreen completed

--- app/src/main/java/com/ingray/samagam/Activity/SplashScreenActivity.kt
+++ app/src/main/java/com/ingray/samagam/Activity/SplashScreenActivity.kt
@@ -1,12 +1,24 @@
 package com.ingray.samagam.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.ingray.samagam.R
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+
+            // Close the splash activity to prevent going back to it on back press
+            finish()
+
+        },SPLASH_TIME_OUT)
     }
 }
