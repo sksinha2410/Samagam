2477bb912bcc99351c79d6d0e321975253d602a0
kush-raj-nit061 <kushpvt1@gmail.com>
2024-01-04 17:39:36 +0530

    errorr fixed

--- app/src/main/java/com/ingray/samagam/Activity/SplashScreenActivity.kt
+++ app/src/main/java/com/ingray/samagam/Activity/SplashScreenActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ingray.samagam.R
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -20,5 +23,20 @@ class SplashScreenActivity : AppCompatActivity() {
             finish()
 
         },SPLASH_TIME_OUT)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = Firebase.auth.currentUser
+        if (currentUser != null) {
+            Toast.makeText(
+                applicationContext,
+                "Entered",
+                Toast.LENGTH_LONG
+            ).show()
+            var intent:Intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
