dd405baf91c94e7b88bed89c876b4fc13530bc63
kush-raj-nit061 <kushpvt1@gmail.com>
2024-01-06 12:50:49 +0530

    shadow added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("net.orandja.shadowlayout:shadowlayout:1.0.1")
 }
