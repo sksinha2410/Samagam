f1e1f333512123aad08966907135674504714b79
kush-raj-nit061 <kushpvt1@gmail.com>
2024-01-08 18:08:21 +0530

    Club event completed

--- app/src/main/java/com/ingray/samagam/Activity/EventsOfClubsActivity.kt
+++ app/src/main/java/com/ingray/samagam/Activity/EventsOfClubsActivity.kt
@@ -1,23 +1,49 @@
 package com.ingray.samagam.Activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+import com.ingray.samagam.Adapters.ClubsAdapter
+import com.ingray.samagam.Adapters.ClubsEventAdapter
+import com.ingray.samagam.DataClass.Clubs
+import com.ingray.samagam.DataClass.Events
 import com.ingray.samagam.R
 
 class EventsOfClubsActivity : AppCompatActivity() {
+    private lateinit var club_event_recycler:RecyclerView
+    private lateinit var clubsEventAdapter: ClubsEventAdapter
+    private lateinit var clubName:TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events_of_clubs)
-        val xmlButton = findViewById<Button>(R.id.xmlButton)
         val itnt = intent
-        var name = itnt.getStringExtra("eventName")
-        xmlButton.setOnClickListener {
-            val xmlLink = "https://www.youtube.com/"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(xmlLink))
-            startActivity(intent)
-        }
+        var name = itnt.getStringExtra("clubName")!!
+
+        club_event_recycler= findViewById(R.id.clubs_event_Recycler)
+        clubName = findViewById(R.id.clubName)
+        clubName.text = name
+        club_event_recycler.itemAnimator = null
+
+        val options: FirebaseRecyclerOptions<Events?> =
+            FirebaseRecyclerOptions.Builder<Events>()
+                .setQuery(
+                    FirebaseDatabase.getInstance().reference.child("Clubs").child(name).child("Events"),
+                    Events::class.java
+                )
+                .build()
+        clubsEventAdapter = ClubsEventAdapter(options)
+        club_event_recycler.adapter = clubsEventAdapter
+        clubsEventAdapter.startListening()
+
+
+
     }
 }
---
+++ app/src/main/java/com/ingray/samagam/Adapters/ClubsEventAdapter.kt
@@ -0,0 +1,74 @@
+package com.ingray.samagam.Adapters
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ingray.samagam.DataClass.Events
+import com.ingray.samagam.R
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+
+class ClubsEventAdapter(options: FirebaseRecyclerOptions<Events?>) :
+    FirebaseRecyclerAdapter<Events?, ClubsEventAdapter.userAdapterHolder?>(options) {
+    override fun onBindViewHolder(
+        holder: userAdapterHolder,
+        position: Int,
+        model: Events
+    ) {
+        holder.event_name.setText(model.event_name)
+        Glide.with(holder.posterImage.context).load(model.purl).into(holder.posterImage)
+
+        holder.reg_link.setOnClickListener {
+            val xmlLink = model.reg_link
+            try {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(xmlLink))
+                holder.itemView.context.startActivity(intent)
+            }catch (e:Exception){
+                Toast.makeText(holder.itemView.context,"No Registration link Available",Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+        holder.brochure_link.setOnClickListener {
+            val xmlLink = model.brochure_link
+            try {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(xmlLink))
+                holder.itemView.context.startActivity(intent)
+            }catch (e:Exception){
+                Toast.makeText(holder.itemView.context,"No Brochure link Available",Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): userAdapterHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.club_event_item,parent,false)
+        return userAdapterHolder(view)
+    }
+
+    inner class userAdapterHolder(innerView:View):RecyclerView.ViewHolder(innerView) {
+        var posterImage:ImageView
+        var event_name:TextView
+        var brochure_link:CardView
+        var reg_link:CardView
+
+        init {
+            posterImage =innerView.findViewById(R.id.event_poster)
+            event_name = innerView.findViewById(R.id.event_name)
+            brochure_link = innerView.findViewById(R.id.rulebook)
+            reg_link = innerView.findViewById(R.id.register)
+        }
+    }
+}
