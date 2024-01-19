4bdbb478081d7c3326559244205b7d87190647c5
aryan <aryankumarchallenger@gmail.com>
2024-01-19 23:18:16 +0530

    club details

---
+++ app/src/main/java/com/ingray/samagam/Activity/ClubDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.ingray.samagam.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ingray.samagam.R
+
+class ClubDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_club_details)
+    }
+}
