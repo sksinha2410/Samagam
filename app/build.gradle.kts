e758d94765a81c60e503c66af84fe167b0b92207
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-10-30 11:04:44 +0530

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,34 +47,34 @@ android {
 
 dependencies {
     // Core Android dependencies
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Firebase dependencies
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.1")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
-    implementation("com.google.firebase:firebase-messaging:23.4.0")
+    implementation("com.google.firebase:firebase-messaging:24.0.3")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
 
     // Shimmer effect library (for Compose and Views)
     implementation ("com.facebook.shimmer:shimmer:0.5.0")
 
     // Jetpack Compose dependencies
-    implementation("androidx.compose.ui:ui:1.5.3")  // Core Compose UI
-    implementation("androidx.compose.material:material:1.5.3")  // Material Design for Compose
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")  // Tooling support
-    implementation("androidx.activity:activity-compose:1.7.2")  // Activity support for Compose
+    implementation("androidx.compose.ui:ui:1.7.4")  // Core Compose UI
+    implementation("androidx.compose.material:material:1.7.4")  // Material Design for Compose
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")  // Tooling support
+    implementation("androidx.activity:activity-compose:1.9.3")  // Activity support for Compose
 
     // Jetpack Compose tooling (Optional for debugging)
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
 
     // Glide for image loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
@@ -87,10 +87,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
 
     // Testing dependencies
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
